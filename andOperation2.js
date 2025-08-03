7400b726b89c5d0a918f7ce67687bd5ec2b5491f
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-03 21:55:39 +0530

    updated

---
+++ andOperation2.js
@@ -0,0 +1,21 @@
+var n1 = 20;
+var n2 = 50;
+var n3 = 68;
+var n4 = 50;
+if (n1 > n2 && n1 > n3) {
+    console.log("n1 is big:" + n1);
+}
+else if (n2 > n1 && n2 > n3) {
+    console.log("n2 is big:" + n2);
+}
+else if (n3 > n1 && n3 > n2) {
+    console.log("n3 is big:" + n3);
+}
+else if (n4 == n1) {
+    console.log("n4, n2 are equeal:" + n3);
+}
+else {
+    console.log("Get Out");
+}
+
+
