b09e7c1f25694b6294e590d91d9784682a151d77
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-05 18:17:50 +0530

    Array

---
+++ array.js
@@ -0,0 +1,11 @@
+var data = ["Apple", "Banana", "Cherry", "Dates", "Starberry"];
+
+console.log("Elements are : ", data);
+
+console.log("Elements 3 is :", data[3]);
+
+console.log("Elememts 4 is :", data[4]);
+
+console.log("Elements 1 is :", data[1]);
+
+console.log("Elements 0 is :", data[0]);
