3ea3037c84d297529ece57ab5f4c6e962399569a
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-05 18:29:06 +0530

    Array2

---
+++ array2.js
@@ -0,0 +1,9 @@
+var data = ["Apple", "Banana", "Cherry", "Date", "Starberry"];
+
+var a = data.length;
+
+console.log("length of the Elements:" + a);
+
+for(a in data){
+    console.log(data[a]);
+}
