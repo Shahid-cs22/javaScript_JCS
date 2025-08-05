62fbe2954166e7cff79d6c5c833494b52cc7b5b7
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-05 18:25:00 +0530

    Multi Dimentional Array

---
+++ multiDimentionalArray.js
@@ -0,0 +1,11 @@
+var data = [["Apple", "Banana", "Cherry", "Orange" ] , [ "Dates", "Starberry", "Mango", "Jack"]];
+
+console.log("Elements in the data:", data);
+
+console.log("Elements in 0 :", data[0]);
+
+console.log("Elements in 1 :", data[1]);
+
+console.log("Elements in 1,3 :", data[1][3]);
+
+console.log("Elements in 0,2 :", data[0][2]);
