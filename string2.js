b9cf96766be8310a77231bbdb840f3eff07fd9a5
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-08 12:05:39 +0530

    sub string

---
+++ string2.js
@@ -0,0 +1,12 @@
+var letter="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+console.log(letter.length);
+
+console.log(letter.slice(4));
+
+console.log(letter.slice(4,8));
+
+console.log(letter.substring(1,10));
+
+console.log(letter.substr(1,10));
