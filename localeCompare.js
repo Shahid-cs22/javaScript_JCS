f12c038c1d1c587f7399db26137737416553ebbe
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-12 20:05:07 +0530

    locale Comapre

---
+++ localeCompare.js
@@ -0,0 +1,16 @@
+/* 
+
+It can compare the value.
+If the two value is same the tesult is 0.
+It the two value is difference the result is -1.
+
+*/
+
+
+var a="apple";
+var b="APPLE";
+var c="apple";
+var d="orange";
+
+console.log("Locale Compare of apple and APPLE is "+ a.localeCompare(b));
+console.log("Locale Compare of apple and apple is "+ a.localeCompare(c));
