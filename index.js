3ca888265690036bba8ef21e75902b6509c48467
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-12 21:56:47 +0530

    indexof search match include

---
+++ index.js
@@ -0,0 +1,17 @@
+var a="Kanyakumari is the southernmost tip of mainland India, in the state of Tamil Nadu, where the Arabian Sea";
+
+console.log(a.indexOf("o"));
+
+console.log(a.lastIndexOf("r"));
+
+console.log(a.search("Tamil"));
+
+console.log(a.includes("Kanyakumari"));
+
+console.log(a.startsWith("w"));
+
+console.log(a.endsWith("r"));
+
+console.log(a.match("india"));
+
+console.log(a.match("India"));
