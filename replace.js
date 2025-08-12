6aa42493e6f23b75992e6371e9a7a6c90e520982
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-12 21:33:45 +0530

    replace and replaceAll

---
+++ replace.js
@@ -0,0 +1,10 @@
+ var a="Good morning, Good evening, good night";
+
+ var b=(a.replace("Good", "bad"));
+ console.log(b);
+
+ console.log("----------------------");
+
+ 
+ var c=(a.replaceAll("Good", "Nothing"));
+ console.log(c);
