1981b5c6439d79438eca8708203ebd9de8342faa
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-12 22:21:50 +0530

    Math operator

---
+++ math.js
@@ -0,0 +1,18 @@
+var x=-56;
+var y= 56;
+var z=56.2548;
+
+
+console.log(Math.abs(x));
+
+console.log(Math.pow(3,3));
+
+console.log(Math.sqrt(8));
+
+console.log(Math.round(z));
+
+console.log(Math.ceil(z));
+
+console.log(Math.floor(z));
+
+console.log(Math.trunc(z));
