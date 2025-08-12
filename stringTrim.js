aa1e857eb3ddd75da37c7a9cba2353c992a3ce0d
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-12 19:52:10 +0530

    The trim() method is used to remove whitespace from both ends of a string.

---
+++ stringTrim.js
@@ -0,0 +1,21 @@
+var text1="         Shahid";
+var text2="     techonology"
+
+
+console.log(text1); 
+console.log("-------------------------");
+
+
+console.log(text2.trim());
+console.log("-------------------------");
+
+
+var x=(text1+text2);
+console.log(x);
+console.log("-------------------------");
+
+
+var y=text1.trim()+text2.trim();
+console.log(y);
+
+// cancel the space
