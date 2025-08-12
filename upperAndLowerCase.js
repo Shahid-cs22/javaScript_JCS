06862d4c3a3dfa9b5478d6b33b89e36193aee012
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-12 21:40:57 +0530

    uppercase and lowercase

---
+++ upperAndLowerCase.js
@@ -0,0 +1,11 @@
+var x="Is useful when you want alphabetical or culturally correct sorting instead of simple Unicode order."
+
+// uppercase
+
+var a=x.toUpperCase();
+console.log(a);
+
+// lowercase
+
+var b=x.toLowerCase();
+console.log(b);
