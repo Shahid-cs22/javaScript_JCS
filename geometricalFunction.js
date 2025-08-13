a841e1a123a6143cd388383015b9b9687e5ee389
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-13 17:20:46 +0530

    grometrical function

---
+++ geometricalFunction.js
@@ -0,0 +1,13 @@
+var x=90;
+
+console.log(Math.sin(x));
+
+console.log(Math.cos(x));
+
+console.log(Math.tan(x));
+
+console.log(Math.asin(x));
+
+console.log(Math.acos(x));
+
+console.log(Math.atan(x));
