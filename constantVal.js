02f7e2283365091115a5737d898fa7bca0ea7049
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-14 18:03:46 +0530

    constant value

---
+++ constantVal.js
@@ -0,0 +1,20 @@
+const a = Math.PI;
+console.log(a);
+
+const b = Math.E;
+console.log(b);
+
+const c = Math.LN2;
+console.log(c);
+
+const d = Math.LN10;
+console.log(d);
+
+const e = Math.LOG2E;
+console.log(e);
+
+const f = Math.SQRT2;
+console.log(f);
+
+const g = Math.SQRT1_2;
+console.log(g);
