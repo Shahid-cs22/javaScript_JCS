223d22775778e64b0d7e60e3f7434f7c237ded65
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-15 16:59:13 +0530

    get date from the user

---
+++ dateGet.js
@@ -0,0 +1,20 @@
+let a = new Date();
+console.log(a);
+
+console.log("month", a.getMonth());
+
+console.log("date", a.getDate());
+
+console.log("day",a.getDay());
+
+console.log("full year", a.getFullYear());
+
+console.log("munites", a.getMinutes());
+
+console.log("millisecond", a.getMilliseconds());
+
+console.log(a.getUTCMilliseconds());
+
+console.log(a.getSeconds());
+
+console.log(a.getTime());
