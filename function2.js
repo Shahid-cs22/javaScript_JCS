3eaba30482fab663591d6440c5262cb958527066
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-15 19:56:42 +0530

    function declaration

---
+++ function2.js
@@ -0,0 +1,11 @@
+//function declaration
+
+
+function num(a,b){
+    console.log("Addof the two numbers is", a+b);
+    console.log("Sub of the two numbers is", a-b);
+    console.log("Mul of the two numbers is", a*b);
+    console.log("Div of the two numbers is", a/b);
+    console.log("Mod of the two numbers is", a%b);
+}
+return num(10,3);
