3f47497ceae6bf46bcd867c020814ffd0e9de50f
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-23 22:39:54 +0530

    keys, values, and entries

---
+++ object9.js
@@ -0,0 +1,20 @@
+let x={
+    name:"shahid",
+    age:21,
+    rollno:38,
+    college:"smce",
+    student:"false"
+}
+
+console.log(Object.keys(x));
+
+console.log("===============");
+
+console.log(Object.values(x));
+
+console.log("===============");
+
+console.log(Object.entries(x));
+
+console.log("===============");
+
