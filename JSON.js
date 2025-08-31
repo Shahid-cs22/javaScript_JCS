11fe0508b148656b2f2c99e23df90be9baf97639
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-08-31 21:48:17 +0530

    JSON 1

---
+++ JSON.js
@@ -0,0 +1,12 @@
+
+
+let details={
+    name:"Mohamed Shahid",
+    age:21,
+    address:"Eathamozhi",
+    phone:7339110968,
+    edu:"BE, CSE",
+    isStudent:true
+};
+
+console.log(JSON.stringify(details));
