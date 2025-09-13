d857ab49bf1c62ab863a2cf727451504d379ba9c
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-09-13 12:11:08 +0530

    ternary operator

---
+++ ternaryOP.js
@@ -0,0 +1,19 @@
+//ternary operator is shortcut of if and else statement.
+// condition ? true statement : false statement
+
+
+let age = 43;
+let message = age>=18 ? "you can vote" : "you are minor";
+console.log(message);
+console.log("-------------");
+
+
+let time = 10;
+let greet = time<12 ? "Good Morning" : "Good Afternoon";
+console.log(greet);
+console.log("-------------");
+
+let isStudent = false;
+let detail = isStudent ? "you are student" : "you are not student";
+console.log(detail);
+console.log("-------------");
