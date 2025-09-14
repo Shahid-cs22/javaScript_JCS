de6ab4fa06ca2e7e422036b19c3dd46b899b3f6b
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-09-14 14:16:34 +0530

    function

---
+++ function.js
@@ -0,0 +1,13 @@
+
+var username = "Mohamed Shahid";
+var age = 21;
+function happyBirthday() {
+
+
+    console.log("Happy Birthday To You");
+    console.log("Happy Birthday To You");
+    console.log(`Happy Birthday dear ${username}!`);
+    console.log("Happy Birthday To You");
+    console.log(`Your age is ${age}`);
+};
+happyBirthday();
